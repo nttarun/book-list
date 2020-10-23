372f66e7d359a65ace2a39efa9d56c2d145af417
nttarun <nttarun719@gmail.com>
2020-10-23 12:44:47 +0530

    initial commit

---
+++ precache-manifest.f68bc84a879ed73c692ef173144b3afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406fe738a8ecc471f94d421a2c10b949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d69c9e0bf6bc1d56d2c",
+    "url": "/static/css/main.3ce9d89e.chunk.css"
+  },
+  {
+    "revision": "0085151f8183fbc3465c",
+    "url": "/static/js/2.e9e3b756.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e9e3b756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d69c9e0bf6bc1d56d2c",
+    "url": "/static/js/main.6af175ed.chunk.js"
+  },
+  {
+    "revision": "b54cedbf1048cef1228c",
+    "url": "/static/js/runtime-main.54ed0bff.js"
+  }
+]);
